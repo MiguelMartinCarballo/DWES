d433c07c5c172cccdadc093b0657f26c161aee69
Miguel Martín <miguelmc.es@gmail.com>
2022-11-02 18:14:54 +0100

    f

---
+++ data/ejercicios/entrega01/ejercicio19 - Miguel Martin/App.php
@@ -0,0 +1,34 @@
+<?php
+
+class App
+{
+  public function __construct()
+  {
+    session_start();    
+  }
+  public function run()
+  {
+    if (isset($_GET['method'])) {
+      $method = $_GET['method'];
+    } else {
+      $method = 'home';
+    }
+  
+    $this->$method();
+  }
+  public function home()
+  {
+    include('views/home.php');
+  }
+
+  public function colores()
+  {
+    include('views/colores.php');
+  }
+
+  public function cambio()
+  {
+    $color = $_GET['color'];
+    setcookie("color", $color, time();
+  }
+}
---
+++ data/ejercicios/entrega01/ejercicio19 - Miguel Martin/views/home.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Colores</h1>
+    Hola
+    <br>
+    <a href="?method=colores">colores </a>
+
+  </ul>
+  </pre>
+</body>
+</html>
